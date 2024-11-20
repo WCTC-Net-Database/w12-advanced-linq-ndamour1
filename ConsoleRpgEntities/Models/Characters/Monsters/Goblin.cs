877a98618b630334b95fae13c676675b331075e4
ndamour1 <143230031+ndamour1@users.noreply.github.com>
2024-11-19 21:47:20 -0500

    Update Goblin.cs

--- ConsoleRpgEntities/Models/Characters/Monsters/Goblin.cs
+++ ConsoleRpgEntities/Models/Characters/Monsters/Goblin.cs
@@ -1,4 +1,4 @@
-﻿using ConsoleRpgEntities.Models.Attributes;
+using ConsoleRpgEntities.Models.Attributes;
 
 namespace ConsoleRpgEntities.Models.Characters.Monsters
 {
@@ -8,8 +8,50 @@ namespace ConsoleRpgEntities.Models.Characters.Monsters
 
         public override void Attack(ITargetable target)
         {
-            // Goblin-specific attack logic
-            Console.WriteLine($"{Name} sneaks up and attacks {target.Name}!");
+            {
+                // "Dice roll"
+                Random rand = new Random();
+                int roll = rand.Next(1, 7);
+
+                // Equipment stats
+                int attack = 0;
+                int defense = 0;
+
+                // Damage calculations
+                decimal damage = 0;
+
+                if (target.Equipment.Armor != null)
+                {
+                    damage = (roll + attack) / ((target.Equipment.Armor.Defense + 100) / 100);
+                }
+                else
+                {
+                    damage = roll + attack;
+                }
+
+                // Total damage
+                int totalDamage = (int)Math.Round(damage, MidpointRounding.AwayFromZero);
+                int overkill = totalDamage + (target.Health - totalDamage);
+
+                // Goblin-specific attack logic
+                Console.WriteLine($"{Name} sneaks up and attacks {target.Name}!");
+                target.Health -= totalDamage;
+                if (target.Health > 0)
+                {
+                    Console.WriteLine($"{target.Name} took {totalDamage} damage.\n{target.Name}'s HP is at {target.Health}.");
+                }
+                else
+                {
+                    if (target.Health - totalDamage < 0)
+                    {
+                        Console.WriteLine($"{target.Name} took {overkill} damage.\n{target.Name} has been defeated.");
+                    }
+                    else if (target.Health - totalDamage == 0)
+                    {
+                        Console.WriteLine($"{target.Name} took {totalDamage} damage.\n{target.Name} has been defeated.");
+                    }
+                }
+            }
         }
     }
 }
