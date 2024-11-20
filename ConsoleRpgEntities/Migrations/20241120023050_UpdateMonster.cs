03602942e6e685eab2338bcdbb754a3813dd4d0b
ndamour1 <143230031+ndamour1@users.noreply.github.com>
2024-11-19 21:32:20 -0500

    Update Monster Migrations

---
+++ ConsoleRpgEntities/Migrations/20241120023050_UpdateMonster.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ConsoleRpgEntities.Migrations
+{
+    public partial class UpdateMonster : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "EquipmentId",
+                table: "Monsters",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Monsters_EquipmentId",
+                table: "Monsters",
+                column: "EquipmentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Monsters_Equipments_EquipmentId",
+                table: "Monsters",
+                column: "EquipmentId",
+                principalTable: "Equipments",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Monsters_Equipments_EquipmentId",
+                table: "Monsters");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Monsters_EquipmentId",
+                table: "Monsters");
+
+            migrationBuilder.DropColumn(
+                name: "EquipmentId",
+                table: "Monsters");
+        }
+    }
+}
