238ff004508f013bfe586c44bf65533d027375d4
ndamour1 <143230031+ndamour1@users.noreply.github.com>
2024-11-19 21:54:03 -0500

    Update ShoveAbility.cs

--- ConsoleRpgEntities/Models/Abilities/PlayerAbilities/ShoveAbility.cs
+++ ConsoleRpgEntities/Models/Abilities/PlayerAbilities/ShoveAbility.cs
@@ -1,4 +1,4 @@
-﻿using ConsoleRpgEntities.Models.Attributes;
+using ConsoleRpgEntities.Models.Attributes;
 using ConsoleRpgEntities.Models.Characters;
 
 namespace ConsoleRpgEntities.Models.Abilities.PlayerAbilities
@@ -10,8 +10,20 @@ namespace ConsoleRpgEntities.Models.Abilities.PlayerAbilities
 
         public override void Activate(IPlayer user, ITargetable target)
         {
+            int totalDamage = Damage + (target.Health - Damage);
+
             // Fireball ability logic
-            Console.WriteLine($"{user.Name} shoves {target.Name} back {Distance} feet, dealing {Damage} damage!");
+            Console.WriteLine($"{user.Name} shoves {target.Name} back {Distance} feet, dealing {totalDamage} damage!");
+            target.Health -= Damage;
+
+            if (target.Health > 0)
+            {
+                Console.WriteLine($"{target.Name}'s HP is at {target.Health}.");
+            }
+            else
+            {
+                Console.WriteLine($"{target.Name} has been defeated.");
+            }
         }
     }
 }
